f0adc8d19cfda6c5ed9e6037c27f5bac513a1589
YisusJuarez <yisusjuarezo@hotmail.com>
2021-03-05 22:32:14 -0600

    Pantalla login responsive

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Login msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+
+</style>
