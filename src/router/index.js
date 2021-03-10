0c5d58da0c84a04d4eb3df990a38fee6cc63efd8
YisusJuarez <yisusjuarezo@hotmail.com>
2021-03-10 14:25:34 -0600

    monitoreo de carriles responsive

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import Login from '@/views/Login.vue'
 import Menu from '@/views/Menu.vue'
 import MonitoreoListas from '@/views/Modules/MonitoreoListas.vue'
+import MonitoreoCarriles from '@/views/Modules/MonitoreoCarriles.vue'
 const routes = [
     {
         path: '/',
@@ -17,6 +18,11 @@ const routes = [
         path: '/inicio/monitoreo-servicios',
         name:'MonitoreoServicios',
         component:MonitoreoListas
+    },
+    {
+        path: '/inicio/monitoreo-carriles',
+        name:'MonitoreoCarriles',
+        component:MonitoreoCarriles
     }
 
 ]
