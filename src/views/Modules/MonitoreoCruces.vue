6ef8b086955389fd0f1954e62c95547b2dfc6305
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2021-10-13 14:38:06 -0500

    Conteo de cruces

--- src/views/Modules/MonitoreoCruces.vue
+++ src/views/Modules/MonitoreoCruces.vue
@@ -9,15 +9,30 @@
       <div class="flex-none mt-1 filter-style">
         <button class="btn-buscar">Buscar</button>
       </div>
-      <div class="flex-none mt-1 ml-right text-white">
+      <!-- <div class="flex-none mt-1 ml-right text-white">
         Tiempo de actualizacion
-        <input type="text" class="text-center" placeholder="3 min">
+        <select v-model="tiempo" @change="tiempos" class="text-gray-800 w-16 rounded">
+          <option value="uno">1 min</option>
+          <option value="dos">2 min</option>
+          <option value="tres">3 min</option>
+        </select>
         No. Transacciones:
-        <input type="text" class="text-center" placeholder="30">
-        <!-- <button class="btn-carriles ml-right">Descargar Excel</button> -->
-      </div>
+        <input type="text" class="text-center rounded" placeholder="30">
+        <button class="btn-carriles ml-right">Descargar Excel</button>
+        <Multiselect v-model="formato" placeholder="Sleccione una Acción" @close="acciones_mapper(formato)" label="name" trackBy="name" :options="opticones_select_acciones()" :searchable="true">
+          <template v-slot:singleLabel="{ value }">
+            <div class="multiselect-single-label">
+              <img height="26" style="margin: 0 6px 0 0;" :src="value.icon"> {{ value.name }}
+            </div>
+          </template>
+          <template v-slot:option="{ option }">
+            <img height="22" style="margin: 0 6px 0 0;" :src="option.icon">{{ option.name }}
+          </template>
+        </Multiselect>
+      </div> -->
     </div>
     <TablaCruces :dataCruces="cruces"></TablaCruces>
+    <p class="mt-10">Próxima actualización en {{ contador.slice(4)  }}</p>
   </div>
   <Footer></Footer>
 </template>
@@ -28,6 +43,7 @@ import TablaCruces from "../../components/Tabla-cruces.vue";
 import Navbar from "../../components/Navbar.vue";
 import Footer from "../../components/Footer-login";
 import axios from "axios";
+import moment from 'moment'
 export default {
   name: "MonitoreoCruces",
   components: { TablaCruces, Navbar, Footer, FormTramoPlaza},
@@ -36,9 +52,15 @@ export default {
       tramo:'',
       plaza:'',
       cruces: [],
+      tiempo:'',
+      contador:'',
+      seconds: 180,
+      formato:''
     };
   },
   beforeMount(){
+    this.contador = moment.utc(this.seconds).format('HH:mm:ss');
+    this.expires_in = this.seconds;
     axios.get(`${API}/Transacciones/Last20Transaccions/3`)
       .then((result)=>{
         console.log(result);
@@ -52,13 +74,73 @@ export default {
           this.cruces.push(obj)
         })
       })
+      this._setInterval()
   },
   methods:{
+    actualizar(){
+      axios.get(`${API}/Transacciones/Last20Transaccions/3`)
+      .then((result)=>{
+        console.log(result);
+        result.data.body.forEach((e) =>{
+          let obj = {
+            carril: e.carril,
+            clase: e.claseCajero,
+            fecha: e.fechaDeCruce,
+            tag: e.idTag
+          }
+          this.cruces.push(obj)
+        })
+      })
+    },
     recibir_tramo_plaza(value){
       this.tramo = value.tramo
       this.plaza = value.plaza
-      console.log(this.plaza);
     },
+    tiempos (){
+      if(this.tiempo == 'uno'){
+        this.seconds = 60
+      }else if(this.tiempo == 'dos'){
+        this.seconds = 120
+      }else if(this.tiempo == 'tres'){
+        this.seconds = 180
+      }
+      this.expires_in = this.seconds;
+    },
+    _setInterval: function() {
+      this.interval = setInterval(() => {
+        if(this.expires_in === 1){
+          this.expires_in = this.seconds
+          this.actualizar()         
+          /* clearInterval(this.interval); */
+        }
+        else {
+          this.expires_in -= 1;
+          this.contador = moment.utc(this.expires_in * 1000).subtract(1, 'seconds').format('HH:mm:ss');                                       
+        }
+      }, 1000);
+    },
+    acciones_mapper(formato){
+      if(formato == 'excel'){
+        console.log('excel');
+      }if(formato == 'csv'){
+        console.log('csv');
+      }if(formato == 'txt'){
+        console.log('txt');
+      }
+      this.formato = ''
+    },
+    opticones_select_acciones(){
+      let options= [
+          {  value: 'excel', name: 'EXCEL'},//0
+          {  value: 'csv', name: 'CSV'},//1
+          {  value: 'txt', name: 'TXT'},//2
+      ]
+      let filtroOpciones = []
+          filtroOpciones.push(options[0])
+          filtroOpciones.push(options[1])
+          filtroOpciones.push(options[2])
+      return filtroOpciones
+    }
   }
 };
 </script>
