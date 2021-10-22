fb607d9c06554bfe1d8c08408ac9255137ed3f19
LaMeraChuleta <geffreyytorres25@gmail.com>
2021-10-22 12:13:01 -0500

    LogSEvice

--- src/Servicios/Files-Service.js
+++ src/Servicios/Files-Service.js
@@ -5,8 +5,7 @@ function xml_hhtp_request(urlToFile,nameFile){
     var oReq = new XMLHttpRequest();  
     oReq.open("GET", urlToFile, true);    
     oReq.responseType = "blob";  
-    let token = ServiceToken.getCookie('Token')
-    console.log(token)
+    let token = ServiceToken.getCookie('Token')    
     oReq.setRequestHeader('Authorization', 'Bearer ' + token);   
     oReq.send();              
     oReq.onload = function () {
