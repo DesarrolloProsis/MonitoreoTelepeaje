dce455e045092df0ed18db27af3e43d86c1d3f79
CarlosPulidoMejia <78100114+CarlosPulidoMejia@users.noreply.github.com>
2022-05-19 17:04:39 -0500

    Estatus Tag con Modal Lista Negra

--- src/components/Tabla-estatustag.vue
+++ src/components/Tabla-estatustag.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Tabla Estatus Tag -->
   <div class="responsive-table">
     <table class="tftable">
       <tr>
@@ -6,6 +7,7 @@
         <th>Estatus</th>
         <th>Saldo</th>
         <th>Tipo Tag</th>
+        <th>Lista Negra</th>
         <th>Última Actualización</th>
       </tr>
       <tr v-for="(tags, index) in datatag" :key="index">
@@ -13,20 +15,60 @@
         <td>{{ tags.estatus }}</td>
         <td>{{ tags.saldo }}</td>
         <td>{{ tags.tipo_tag }}</td>
+        <td>
+            <img class="img-search" src="~@/assets/opcion.png" @click="abrirModal(tags)"/>
+        </td>
         <td>{{ moment(tags.ult_act).format("YYYY-MM-DD  HH:mm:ss a") }}</td>
       </tr>
     </table>
   </div>
+  <!-- Modal Información Lista Negra -->
+  <div class="sticky inset-0 " :class="{'modal-container': modalListaNegra}">
+    <div v-if="modalListaNegra" class="rounded-lg  justify-center border absolute inset-x-0 bg-white border-gray-400 w-69  mx-auto px-12 py-10 shadow-2xl mt-66">
+      <p class="text-gray-900 font-bold text-2xl -mt-8 mb-8 text-center">Información de Lista Negra</p>     
+      <div class="grid grid-cols-2 mt-2">
+        <p class="text-sm mb-1 font-semibold text-gray-700 sm:-ml-6">Fecha Entrada</p>
+        <!-- <input v-model="newRol.nombre" type="text" class="border rounded-lg"> -->
+        <span>{{ moment(fechaEntrada).format("YYYY-MM-DD  HH:mm:ss a") }}</span>
+        <p class="text-sm mb-1 font-semibold text-gray-700 sm:-ml-6">Fecha Salida</p>
+        <span>{{ moment(fechaSalida).format("YYYY-MM-DD  HH:mm:ss a") }}</span>
+        <!-- <input v-model="newRol.nombre" type="text" class="border rounded-lg"> -->
+        <p class="text-sm mb-1 font-semibold text-gray-700 sm:-ml-6">Tipo de Causa</p>
+        <span>{{ tipoCausa }}</span>
+        <p class="text-sm mb-1 font-semibold text-gray-700 sm:-ml-6">Descripción de la Causa</p>
+        <span>{{ descripcionCausa }}</span>
+        <!-- <input v-model="newRol.nombre" type="text" class="border rounded-lg"> -->
+      </div>
+      <div class="mt-5 text-center ml-6">
+        <button @click="modalListaNegra = false" class="botonIconOk">Ok</button>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-import moment from 'moment'
+import { ref } from 'vue' // se importa para poder generar una referencia reactiva para tipos primitivos (números, strings o booleanos)
+import moment from 'moment' //
 export default {
-  name: "TablaEstatusTag",
-  props: ["datatag"],
+  name: "TablaEstatusTag",//Nombre del componente para mostrar en vuex
+  props: ["datatag"],//propiedad que trae toda la infromación sobre el tag
   created: function () {
-    this.moment = moment;
+    this.moment = moment; //moment funciona para dar un formato a la fecha
   },
+  setup(){
+    const modalListaNegra = ref(false) // variable que indica si se abre el modal de la información de la lista negra
+    const fechaEntrada = ref('') //variable que indica la fecha de entrada a la lista negra
+    const fechaSalida = ref('') //variable que indica la fecha de salida de la lista negra
+    const tipoCausa = ref('') //variable que indica el tipo de causa de la entrada a lista negra
+    const descripcionCausa = ref('') //variable que indica la descripción de la causa
+    function abrirModal (tags) { //función para abrir el modal de la infromación de la lista negra
+      fechaEntrada.value = tags.ult_act //asignación de valor de la fecha de entrada
+      fechaSalida.value = tags.ult_act //asignación de valor de la fecha de salida
+      tipoCausa.value = tags.tipo_tag // asignación de valor del tipo de causa de entrada a la lista negra
+      descripcionCausa.value = tags.estatus // asignación de valor de la descripción de la causa
+      modalListaNegra.value = true //se abre el modal
+    }
+    return { modalListaNegra, fechaEntrada, fechaSalida, tipoCausa, descripcionCausa, abrirModal }
+  }
 };
 </script>
 <style scoped>
@@ -69,5 +111,17 @@ export default {
   border-right-color: white;
 
   text-align: center;
+}
+.img-search {
+  display: inline;
+  width: 20px;
+  cursor: pointer;
+}
+.modal-container{
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    z-index: 1000;
+    background: rgba(0, 0, 0, 0.5);
 }
 </style>
